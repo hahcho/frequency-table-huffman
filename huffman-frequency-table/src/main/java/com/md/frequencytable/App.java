faa28e2a9aa41a2af1d8f0071634f57b34a0bf78
Mario Daskalov <mario@browswave.com>
2015-06-27 01:28:27 +0300

    Move paralesation logic to FrequencyCounter

--- huffman-frequency-table/src/main/java/com/md/frequencytable/App.java
+++ huffman-frequency-table/src/main/java/com/md/frequencytable/App.java
@@ -18,51 +18,14 @@ public class App {
 
         File file = new File(arguments.filepath());
 
-        ExecutorService executor = Executors.newFixedThreadPool(arguments.numberOfThreads());
-
         long start = Calendar.getInstance().getTimeInMillis();
-
-        Collection<Future<Map<Character, Long>>> frequencyTables = executor.invokeAll(buildCallables(file, arguments.numberOfThreads()));
-
-        Map<Character, Long> accumulatedFrequencyTable = new HashMap<Character, Long>();
-        for(Future<Map<Character, Long>> frequencyTableFuture : frequencyTables) {
-            Map<Character, Long> frequencyTable = frequencyTableFuture.get();
-            for(Character key : frequencyTable.keySet()) {
-                Long frequencyForCharacter = frequencyTable.get(key);
-                Long currentFrequencyForCharacter = accumulatedFrequencyTable.get(key);
-                if(currentFrequencyForCharacter == null) {
-                    currentFrequencyForCharacter = 0l;
-                }
-                accumulatedFrequencyTable.put(key, currentFrequencyForCharacter + frequencyForCharacter);
-            }
-        }
-
+        Map<Character, Long> accumulatedFrequencyTable = FrequencyCounter.parallelCount(file, arguments.numberOfThreads());
         long end = Calendar.getInstance().getTimeInMillis();
+
         System.out.println("Time taken: " + ((end - start) / 100));
         printFrequencyTable(accumulatedFrequencyTable);
-        executor.shutdown();
     }
 
-    private static Collection<Callable<Map<Character, Long>>> buildCallables(File file, int numberOfThreads) {
-        long fileLength = file.length();
-        long chunkSize;
-
-        if(fileLength % numberOfThreads == 0) {
-            chunkSize = fileLength / numberOfThreads;
-        } else {
-            chunkSize = fileLength / (numberOfThreads - 1);
-        }
-        List<Callable<Map<Character, Long>>> callables = new ArrayList<Callable<Map<Character, Long>>>();
-
-        for(int i = 0; i < (numberOfThreads - 1); i++) {
-            callables.add(new FrequencyCounter(file, i * chunkSize, chunkSize));
-        }
-
-        long remainingStart = (numberOfThreads - 1) * chunkSize;
-        callables.add(new FrequencyCounter(file, remainingStart, fileLength - remainingStart));
-
-        return callables;
-    }
 
     private static void printFrequencyTable(Map<Character, Long> frequencyTable) {
         List<Character> sortedCharacters = new ArrayList<Character>(frequencyTable.keySet());
--- huffman-frequency-table/src/main/java/com/md/frequencytable/FrequencyCounter.java
+++ huffman-frequency-table/src/main/java/com/md/frequencytable/FrequencyCounter.java
@@ -37,4 +37,47 @@ public class FrequencyCounter implements Callable<Map<Character, Long>> {
  
         return frequencyTable;
     }
+
+    public static Map<Character, Long> parallelCount(File file, int numberOfThreads) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+        Collection<Future<Map<Character, Long>>> frequencyTables = executor.invokeAll(buildCallables(file, numberOfThreads));
+
+        Map<Character, Long> accumulatedFrequencyTable = new HashMap<Character, Long>();
+        for(Future<Map<Character, Long>> frequencyTableFuture : frequencyTables) {
+            Map<Character, Long> frequencyTable = frequencyTableFuture.get();
+            for(Character key : frequencyTable.keySet()) {
+                Long frequencyForCharacter = frequencyTable.get(key);
+                Long currentFrequencyForCharacter = accumulatedFrequencyTable.get(key);
+                if(currentFrequencyForCharacter == null) {
+                    currentFrequencyForCharacter = 0l;
+                }
+                accumulatedFrequencyTable.put(key, currentFrequencyForCharacter + frequencyForCharacter);
+            }
+        }
+
+        executor.shutdown();
+
+        return accumulatedFrequencyTable;
+    }
+
+    private static Collection<Callable<Map<Character, Long>>> buildCallables(File file, int numberOfThreads) {
+        long fileLength = file.length();
+        long chunkSize;
+
+        if(fileLength % numberOfThreads == 0) {
+            chunkSize = fileLength / numberOfThreads;
+        } else {
+            chunkSize = fileLength / (numberOfThreads - 1);
+        }
+        List<Callable<Map<Character, Long>>> callables = new ArrayList<Callable<Map<Character, Long>>>();
+
+        for(int i = 0; i < (numberOfThreads - 1); i++) {
+            callables.add(new FrequencyCounter(file, i * chunkSize, chunkSize));
+        }
+
+        long remainingStart = (numberOfThreads - 1) * chunkSize;
+        callables.add(new FrequencyCounter(file, remainingStart, fileLength - remainingStart));
+
+        return callables;
+    }
 }
